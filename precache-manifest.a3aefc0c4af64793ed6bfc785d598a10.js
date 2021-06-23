e0de0adc1a2e6d17e953bf57967b4faac1048310
m666362 <mdrayhan9464@gmail.com>
2021-06-23 22:23:23 +0600

    Updates

---
+++ precache-manifest.a3aefc0c4af64793ed6bfc785d598a10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f3facc633c0a0972966",
+    "url": "/Indicision/static/css/main.d6c63a18.chunk.css"
+  },
+  {
+    "revision": "2f3facc633c0a0972966",
+    "url": "/Indicision/static/js/main.2f3facc6.chunk.js"
+  },
+  {
+    "revision": "7f6874fc654348c3ae6c",
+    "url": "/Indicision/static/css/1.71ebd4cf.chunk.css"
+  },
+  {
+    "revision": "7f6874fc654348c3ae6c",
+    "url": "/Indicision/static/js/1.7f6874fc.chunk.js"
+  },
+  {
+    "revision": "b7eb2597dca0ebe381f1",
+    "url": "/Indicision/static/js/runtime~main.b7eb2597.js"
+  },
+  {
+    "revision": "ba4450215e33bacddec4c34cdc3d6dc1",
+    "url": "/Indicision/index.html"
+  }
+];
